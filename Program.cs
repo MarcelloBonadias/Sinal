6598b88638f53021fe9bb9f109b15a2606f7f91b
Marcello Bonadias <marcelloebonadias@gmail.com>
2022-04-01 21:57:34 -0300

    Finalizar Tarefa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int numeroDigitado;
+
+Console.WriteLine("Digite um número, por favor");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDigitado == 0)
+{
+    Console.WriteLine ("Zero");
+} 
+
+ else if (numeroDigitado < 0 )
+{
+    Console.WriteLine("Negativo");
+}
+
+if (numeroDigitado > 0) 
+{
+    Console.WriteLine("Positivo");
+}
